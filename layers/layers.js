d6d52e4fa3ff2027fd2a227e13a7d23a92e39e90
filip-nikolic <111447879+filip-nikolic@users.noreply.github.com>
2025-11-19 23:51:50 +0100

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -1,3 +1,5 @@
+ol.proj.proj4.register(proj4);
+//ol.proj.get("EPSG:4326").setExtent([12.646431, 52.686653, 12.694087, 52.723146]);
 var wms_layers = [];
 
 
@@ -13,7 +15,7 @@ var wms_layers = [];
         });
 var format_LWL_Plan_1 = new ol.format.GeoJSON();
 var features_LWL_Plan_1 = format_LWL_Plan_1.readFeatures(json_LWL_Plan_1, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
 var jsonSource_LWL_Plan_1 = new ol.source.Vector({
     attributions: ' ',
 });
@@ -29,7 +31,7 @@ var lyr_LWL_Plan_1 = new ol.layer.Vector({
     <img src="styles/legend/LWL_Plan_1_1.png" /> Bestand<br />' });
 var format_Verllegprotokol_2 = new ol.format.GeoJSON();
 var features_Verllegprotokol_2 = format_Verllegprotokol_2.readFeatures(json_Verllegprotokol_2, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
 var jsonSource_Verllegprotokol_2 = new ol.source.Vector({
     attributions: ' ',
 });
@@ -45,7 +47,7 @@ var lyr_Verllegprotokol_2 = new ol.layer.Vector({
     <img src="styles/legend/Verllegprotokol_2_1.png" /> verlegt<br />' });
 var format_VAMlocations_3 = new ol.format.GeoJSON();
 var features_VAMlocations_3 = format_VAMlocations_3.readFeatures(json_VAMlocations_3, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
 var jsonSource_VAMlocations_3 = new ol.source.Vector({
     attributions: ' ',
 });
@@ -60,7 +62,7 @@ var lyr_VAMlocations_3 = new ol.layer.Vector({
             });
 var format_DB_GeoStreckennetz_4 = new ol.format.GeoJSON();
 var features_DB_GeoStreckennetz_4 = format_DB_GeoStreckennetz_4.readFeatures(json_DB_GeoStreckennetz_4, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
 var jsonSource_DB_GeoStreckennetz_4 = new ol.source.Vector({
     attributions: ' ',
 });
@@ -75,7 +77,7 @@ var lyr_DB_GeoStreckennetz_4 = new ol.layer.Vector({
             });
 var format_Locations_new_5 = new ol.format.GeoJSON();
 var features_Locations_new_5 = format_Locations_new_5.readFeatures(json_Locations_new_5, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
 var jsonSource_Locations_new_5 = new ol.source.Vector({
     attributions: ' ',
 });
@@ -102,11 +104,11 @@ lyr_Verllegprotokol_2.set('fieldImages', {'id': 'TextEdit', 'Baustelle': 'TextEd
 lyr_VAMlocations_3.set('fieldImages', {'fid': 'TextEdit', 'VAM_ID': 'TextEdit', 'VAM_Km': 'TextEdit', 'Braitengrad': 'TextEdit', 'Langengrad': 'TextEdit', });
 lyr_DB_GeoStreckennetz_4.set('fieldImages', {'mifcode': 'TextEdit', 'strecke_nr': 'TextEdit', 'richtung': 'TextEdit', 'laenge': 'TextEdit', 'von_km_i': 'TextEdit', 'bis_km_i': 'TextEdit', 'von_km_l': 'TextEdit', 'bis_km_l': 'TextEdit', 'elektrifiz': 'TextEdit', 'bahnnutzun': 'TextEdit', 'geschwindi': 'TextEdit', 'strecke_ku': 'TextEdit', 'gleisanzah': 'TextEdit', 'bahnart': 'TextEdit', 'kmspru_typ': 'TextEdit', 'kmspru_t00': 'TextEdit', });
 lyr_Locations_new_5.set('fieldImages', {'Objektkennung': 'TextEdit', 'Objekt': 'TextEdit', 'Aktive-Alternative': 'TextEdit', 'Kilometer': 'TextEdit', 'Bundesland': 'TextEdit', 'Landkreis': 'TextEdit', 'Region': 'TextEdit', 'Objekt Lage am Gleis': 'TextEdit', 'Objekthöhe': 'TextEdit', 'Höhe über NN': 'TextEdit', 'Abstand OM zu Gleismitte': 'TextEdit', 'Abstand OM zu Grundstücksgrenze': 'TextEdit', 'Böschung?': 'TextEdit', 'Objekt-Fundamentgröße': 'TextEdit', 'KFS vorhanden': 'TextEdit', 'TB-Abschnitt': 'TextEdit', 'Längengrad': 'TextEdit', 'Breitengrad': 'TextEdit', 'Bau (Zugang nur Gleis)?': 'TextEdit', 'GSM-R': 'TextEdit', 'Umweltschutz': 'TextEdit', 'Kampfmittelverdacht': 'TextEdit', 'Datum-KMB': 'TextEdit', 'Az-KMB': 'TextEdit', 'Empfehlung KMB': 'TextEdit', 'Baulos': 'TextEdit', 'Baukoordinator': 'TextEdit', 'Bombardierung': 'TextEdit', 'Gemeinde': 'TextEdit', 'Kleinkampfmittel': 'TextEdit', 'Aktueller Status': 'TextEdit', 'Höhe über OK-Schiene': 'TextEdit', 'Flur': 'Range', 'Flurstück': 'TextEdit', 'Gemarkung': 'TextEdit', 'Reptilien': 'TextEdit', 'Amphibien': 'TextEdit', 'Abstand z. Gleisachse': 'TextEdit', 'Freeze-Status': 'TextEdit', });
-lyr_LWL_Plan_1.set('fieldLabels', {'id': 'no label', 'LWL_Plan': 'no label', 'Cable': 'no label', 'Status': 'no label', });
-lyr_Verllegprotokol_2.set('fieldLabels', {'id': 'no label', 'Baustelle': 'no label', 'Kabelnum': 'no label', 'Kabeltype': 'no label', 'radius(mm)': 'no label', 'Trommelnum': 'no label', 'von Ort': 'no label', 'von km': 'no label', 'Metrier': 'no label', 'bis Ort': 'no label', 'bis km': 'no label', 'bis Metr': 'no label', 'KLangeSOLL': 'no label', 'KLangeIST': 'no label', 'Recht/Link': 'no label', 'Besonderh.': 'no label', });
-lyr_VAMlocations_3.set('fieldLabels', {'fid': 'no label', 'VAM_ID': 'no label', 'VAM_Km': 'no label', 'Braitengrad': 'no label', 'Langengrad': 'no label', });
+lyr_LWL_Plan_1.set('fieldLabels', {'id': 'hidden field', 'LWL_Plan': 'hidden field', 'Cable': 'inline label - visible with data', 'Status': 'inline label - visible with data', });
+lyr_Verllegprotokol_2.set('fieldLabels', {'id': 'hidden field', 'Baustelle': 'hidden field', 'Kabelnum': 'inline label - always visible', 'Kabeltype': 'inline label - always visible', 'radius(mm)': 'hidden field', 'Trommelnum': 'hidden field', 'von Ort': 'inline label - visible with data', 'von km': 'inline label - visible with data', 'Metrier': 'inline label - visible with data', 'bis Ort': 'inline label - always visible', 'bis km': 'inline label - visible with data', 'bis Metr': 'inline label - visible with data', 'KLangeSOLL': 'hidden field', 'KLangeIST': 'hidden field', 'Recht/Link': 'hidden field', 'Besonderh.': 'inline label - visible with data', });
+lyr_VAMlocations_3.set('fieldLabels', {'fid': 'hidden field', 'VAM_ID': 'inline label - always visible', 'VAM_Km': 'inline label - always visible', 'Braitengrad': 'inline label - always visible', 'Langengrad': 'inline label - always visible', });
 lyr_DB_GeoStreckennetz_4.set('fieldLabels', {'mifcode': 'no label', 'strecke_nr': 'no label', 'richtung': 'no label', 'laenge': 'no label', 'von_km_i': 'no label', 'bis_km_i': 'no label', 'von_km_l': 'no label', 'bis_km_l': 'no label', 'elektrifiz': 'no label', 'bahnnutzun': 'no label', 'geschwindi': 'no label', 'strecke_ku': 'no label', 'gleisanzah': 'no label', 'bahnart': 'no label', 'kmspru_typ': 'no label', 'kmspru_t00': 'no label', });
-lyr_Locations_new_5.set('fieldLabels', {'Objektkennung': 'no label', 'Objekt': 'no label', 'Aktive-Alternative': 'no label', 'Kilometer': 'no label', 'Bundesland': 'no label', 'Landkreis': 'no label', 'Region': 'no label', 'Objekt Lage am Gleis': 'no label', 'Objekthöhe': 'no label', 'Höhe über NN': 'no label', 'Abstand OM zu Gleismitte': 'no label', 'Abstand OM zu Grundstücksgrenze': 'no label', 'Böschung?': 'no label', 'Objekt-Fundamentgröße': 'no label', 'KFS vorhanden': 'no label', 'TB-Abschnitt': 'no label', 'Längengrad': 'no label', 'Breitengrad': 'no label', 'Bau (Zugang nur Gleis)?': 'no label', 'GSM-R': 'no label', 'Umweltschutz': 'no label', 'Kampfmittelverdacht': 'no label', 'Datum-KMB': 'no label', 'Az-KMB': 'no label', 'Empfehlung KMB': 'no label', 'Baulos': 'no label', 'Baukoordinator': 'no label', 'Bombardierung': 'no label', 'Gemeinde': 'no label', 'Kleinkampfmittel': 'no label', 'Aktueller Status': 'no label', 'Höhe über OK-Schiene': 'no label', 'Flur': 'no label', 'Flurstück': 'no label', 'Gemarkung': 'no label', 'Reptilien': 'no label', 'Amphibien': 'no label', 'Abstand z. Gleisachse': 'no label', 'Freeze-Status': 'no label', });
+lyr_Locations_new_5.set('fieldLabels', {'Objektkennung': 'inline label - always visible', 'Objekt': 'hidden field', 'Aktive-Alternative': 'hidden field', 'Kilometer': 'inline label - always visible', 'Bundesland': 'hidden field', 'Landkreis': 'hidden field', 'Region': 'hidden field', 'Objekt Lage am Gleis': 'inline label - visible with data', 'Objekthöhe': 'inline label - visible with data', 'Höhe über NN': 'hidden field', 'Abstand OM zu Gleismitte': 'hidden field', 'Abstand OM zu Grundstücksgrenze': 'hidden field', 'Böschung?': 'hidden field', 'Objekt-Fundamentgröße': 'hidden field', 'KFS vorhanden': 'hidden field', 'TB-Abschnitt': 'hidden field', 'Längengrad': 'inline label - visible with data', 'Breitengrad': 'inline label - always visible', 'Bau (Zugang nur Gleis)?': 'hidden field', 'GSM-R': 'hidden field', 'Umweltschutz': 'hidden field', 'Kampfmittelverdacht': 'hidden field', 'Datum-KMB': 'hidden field', 'Az-KMB': 'hidden field', 'Empfehlung KMB': 'hidden field', 'Baulos': 'hidden field', 'Baukoordinator': 'hidden field', 'Bombardierung': 'hidden field', 'Gemeinde': 'hidden field', 'Kleinkampfmittel': 'hidden field', 'Aktueller Status': 'hidden field', 'Höhe über OK-Schiene': 'hidden field', 'Flur': 'hidden field', 'Flurstück': 'hidden field', 'Gemarkung': 'hidden field', 'Reptilien': 'hidden field', 'Amphibien': 'hidden field', 'Abstand z. Gleisachse': 'hidden field', 'Freeze-Status': 'hidden field', });
 lyr_Locations_new_5.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
